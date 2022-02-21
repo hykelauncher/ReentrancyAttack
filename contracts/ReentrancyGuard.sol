295ae6c0f62fe48d6399d22e0005f65481008b78
m r <trash89@laposte.net>
2022-02-21 17:24:05 +0100

    add noReentrancy modifier
    modifs deployment scripts

---
+++ contracts/ReentrancyGuard.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract ReentrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+}
